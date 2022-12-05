36df91ff60c99397f45490ab5ebf157aebcaac4a
valenn0101 <veroncaceresvalentin@gmail.com>
2022-12-04 21:28:04 -0300

    Tarea 4 completada

--- Tarea 4/Tarea-4.js
+++ Tarea 4/Tarea-4.js
@@ -1,9 +1,76 @@
+const $botonCalcular = document.getElementById('calcular');
+
+const numeros = document.querySelectorAll('li');
+const listadoDeNumeros = obtenerNumeros();
+const promedios = calcularPromedios(listadoDeNumeros);
+const numeroMayor = mostrarMayor(listadoDeNumeros);
+const numeroMenor = mostrarMenor(listadoDeNumeros);
+const masRepetido = mostrarReptido(listadoDeNumeros);
+
+const $promedio = document.getElementById('promedio');
+const $menor = document.getElementById('pequeño');
+const $mayor = document.getElementById('grande');
+const $repetido = document.getElementById('repetido');
 
 
-//TAREA 4: En otro archivo distinto,
-// Crear una lista de <ol> y <li> que contengan sólo números.
-// Convertir esos números a un array y:
-// 1. calcular el promedio y mostrarlo en un <em> pre-creado con el texto "El promedio es..."
-// 2. obtener el número más pequeño y mostrarlo en un <em> pre-creado con el texto "El número más pequeño es..."
-// 3. obtener el número más grande y mostrarlo en un <em> pre-creado con el texto "El número más grande es..."
-// 4. obtener el número que más se repite y mostrarlo en un <em> pre-creado con el texto "El número más frecuente es..."
+function obtenerNumeros (){
+    const numerosTotales = [];
+    for (let i = 0; i < numeros.length; i++){
+        numerosTotales.push(Number(numeros[i].innerText))
+    }
+    return numerosTotales;
+}
+
+console.log(listadoDeNumeros);
+
+function calcularPromedios(listadoDeNumeros) {
+    let sum = 0;
+    for (let i = 0; i < listadoDeNumeros.length; i++) {
+        sum += listadoDeNumeros[i];
+    }
+    return sum / listadoDeNumeros.length;
+}
+console.log(promedios);
+
+function mostrarMayor(listadoDeNumeros){
+    let mayor = listadoDeNumeros[0];
+    for(let i = 0; i < listadoDeNumeros.length; i++){
+        if(listadoDeNumeros[i] > mayor){
+            mayor = listadoDeNumeros[i];
+        }
+    }
+    return mayor;
+}
+console.log(numeroMayor);
+
+function mostrarMenor(listadoDeNumeros){
+    let menor = listadoDeNumeros[0];
+    for(let i = 0; i < listadoDeNumeros.length; i++){
+        if(listadoDeNumeros[i]< menor){
+            menor = listadoDeNumeros[i];
+        }
+    }
+    return menor;
+}
+
+console.log(numeroMenor);
+
+function mostrarReptido(listadoDeNumeros){
+    let repetido = listadoDeNumeros[0];
+    for(let i = 0; i < listadoDeNumeros.length; i++){
+        for(let j = i + 1; j <listadoDeNumeros.length; j++){
+            if (listadoDeNumeros[i] === listadoDeNumeros[j]){
+                repetido = listadoDeNumeros[j]
+            }
+        }
+    }
+    return repetido;
+}
+console.log(masRepetido);
+
+$botonCalcular.onclick = function(){
+    $promedio.innerText = promedios;
+    $menor.innerText = numeroMenor;
+    $mayor.innerText = numeroMayor;
+    $repetido.innerText = masRepetido;
+}
